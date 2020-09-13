16c0d101bd9ea77c67927867f614e65521fd98d5
Ignacio Sanchez Gines <nacho@geardome.com>
2020-09-13 14:37:24 +0200

    Improve error handling

--- internal/clients/user.go
+++ internal/clients/user.go
@@ -2,9 +2,11 @@ package clients
 
 import (
 	"log"
+	"time"
 
 	"github.com/drhelius/grpc-demo-proto/user"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/keepalive"
 )
 
 var UserService user.UserServiceClient
@@ -12,7 +14,13 @@ var UserService user.UserServiceClient
 func init() {
 	log.Printf("[Account] Dialing to 'user:5000' ...")
 
-	conn, err := grpc.Dial("user:5000", grpc.WithInsecure(), grpc.WithBlock(), grpc.FailOnNonTempDialError(true))
+	keepAliveParams := keepalive.ClientParameters{
+		Time:                5 * time.Second,
+		Timeout:             time.Second,
+		PermitWithoutStream: true,
+	}
+
+	conn, err := grpc.Dial("user:5000", grpc.WithInsecure(), grpc.WithBlock(), grpc.FailOnNonTempDialError(true), grpc.WithKeepaliveParams(keepAliveParams))
 	if err != nil {
 		log.Fatalf("[Account] Error dialing to User service: %v", err)
 	}
