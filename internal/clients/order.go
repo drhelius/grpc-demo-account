a5a0016381ad100b83c1316e7afee0153c00610a
Ignacio Sanchez Gines <nacho@geardome.com>
2020-09-13 15:12:38 +0200

    Improve error handling

--- internal/clients/order.go
+++ internal/clients/order.go
@@ -2,9 +2,11 @@ package clients
 
 import (
 	"log"
+	"time"
 
 	"github.com/drhelius/grpc-demo-proto/order"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/keepalive"
 )
 
 var OrderService order.OrderServiceClient
@@ -12,7 +14,13 @@ var OrderService order.OrderServiceClient
 func init() {
 	log.Printf("[Account] Dialing to 'order:5000' ...")
 
-	conn, err := grpc.Dial("order:5000", grpc.WithInsecure(), grpc.WithBlock(), grpc.FailOnNonTempDialError(true))
+	keepAliveParams := keepalive.ClientParameters{
+		Time:                5 * time.Second,
+		Timeout:             time.Second,
+		PermitWithoutStream: true,
+	}
+
+	conn, err := grpc.Dial("order:5000", grpc.WithInsecure(), grpc.WithBlock(), grpc.FailOnNonTempDialError(true), grpc.WithKeepaliveParams(keepAliveParams))
 	if err != nil {
 		log.Fatalf("[Account] Error dialing to Order service: %v", err)
 	}
